7cb9626155c00566d1d22419001ec5fafde59b77
chenmei17 <cocoa2675@gmail.com>
2026-03-18 14:42:46 +0900

    feat: split admin-web layout and page shells into clean branch (#56)

--- admin-web/src/app/router/AdminRoutes.jsx
+++ admin-web/src/app/router/AdminRoutes.jsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import AdminAuditPage from "../../pages/AdminAuditPage.jsx";
+import BatchPage from "../../pages/admin/BatchPage.jsx"
+import SettlementManagePage from "../../pages/admin/SettlementManagePage.jsx"
+import SettlementDetailAdminPage from "../../pages/admin/SettlementDetailAdminPage.jsx"
 
 export default function AdminRoutes() {
   return (
     <Routes>
       <Route path="audit" element={<AdminAuditPage />} />
+      <Route path="settlement-batches" element={<BatchPage />}/>
+      <Route path="settlements" element={<SettlementManagePage />}/>
+      <Route
+        path="settlements/:settlementId"
+        element={<SettlementDetailAdminPage />}      
+      />
     </Routes>
   );
 }
