e58c80a6c23cf0c03d17a9ecba5528de9c11bbbd
chenmei17 <cocoa2675@gmail.com>
2026-03-18 14:14:41 +0900

    feat: split admin-web layout and page shells into clean branch

--- admin-web/src/app/router/ConsumerRoutes.jsx
+++ admin-web/src/app/router/ConsumerRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import OrderCreatePage from "../../pages/consumer/OrderCreatePage.jsx";
+import MyOrdersPage from "../../pages/consumer/MyOrdersPage.jsx";
 
 export default function ConsumerRoutes() {
     return (
         <Routes>
             <Route path="orders/new" element={<OrderCreatePage />} />
+            <Route path="orders" element={<MyOrdersPage />} />
         </Routes>
     );
 }
---
+++ admin-web/src/pages/merchant/SettlementDetailPage.jsx
@@ -0,0 +1,3 @@
+export default function SettlementDetailPage() {
+  return <div>SettlementDetailPage</div>;
+}
