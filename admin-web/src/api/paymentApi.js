1b37c40bae445a1ebfe27a87f3edf1b1b46314cc
chenmei17 <cocoa2675@gmail.com>
2026-03-20 18:13:13 +0900

    feat(dev-login): 개발용 역할별 세션 로그인 페이지 및 로그아웃 UI 추가

--- admin-web/src/api/paymentApi.js
+++ admin-web/src/api/paymentApi.js
@@ -3,6 +3,13 @@
 import { requestJson } from "./http.js";
 
 // TODO: C2(B 담당)에서 구현
+
+// order detail page load
+export function getConsumerOrderDetail(orderId) {
+  return requestJson(`/api/consumer/orders/${orderId}`);
+}
+
+// 구매확정 pay 실행
 export function payOrder(orderId, idempotencyKey) {
     return requestJson(`/api/consumer/orders/${orderId}/pay`, {
         method: "POST",
