d7fc68995ce903eca64ed8c627f7c206708ebab7
chenmei17 <cocoa2675@gmail.com>
2026-03-22 18:07:22 +0900

    Update format.js
    주석 변경

--- admin-web/src/util/format.js
+++ admin-web/src/util/format.js
@@ -1,5 +1,5 @@
-// /admin-web/src/utils/format.js
-// 사용: import { formatDateTime, formatDateTimeWithSeconds, formatKrw } from "../../utils/format.js";
+// /admin-web/src/util/format.js
+// 사용: import { formatDateTime, formatDateTimeWithSeconds, formatKrw } from "../../util/format.js";
 
 // yyyy-MM-dd HH:mm
 export function formatDateTime(value) {
